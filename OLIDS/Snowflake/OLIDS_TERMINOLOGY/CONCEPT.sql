48db5ec18c9664549344d79a9073782a7f97c0e8
Bill Wood <william.wood1@nelcsu.nhs.uk>
2025-04-22 23:38:36 +0100

    Add OLIDS_TERMINOLOGY tables for CONCEPT and CONCEPT_MAP

---
+++ OLIDS/Snowflake/OLIDS_TERMINOLOGY/CONCEPT.sql
@@ -0,0 +1,12 @@
+create or replace TABLE "Data_Store_OLIDS_Dummy".OLIDS_TERMINOLOGY.CONCEPT (
+	"id" VARCHAR(16777216),
+	"lds_id" VARCHAR(16777216),
+	"lds_business_key" VARCHAR(16777216),
+	"lds_dataset_id" VARCHAR(16777216),
+	"system" VARCHAR(16777216),
+	"code" VARCHAR(16777216),
+	"display" VARCHAR(16777216),
+	"is_mapped" BOOLEAN,
+	"use_count" NUMBER(38,0),
+	"lds_start_date_time" TIMESTAMP_NTZ(9)
+);
